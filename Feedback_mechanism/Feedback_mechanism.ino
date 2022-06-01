1deb06423012b7cf364fbf67df6fbfa4afc99309
Boody450 <92160604+Boody450@users.noreply.github.com>
2022-06-01 08:48:09 +0200

    Update Feedback_mechanism.ino

--- Feedback_mechanism/Feedback_mechanism.ino
+++ Feedback_mechanism/Feedback_mechanism.ino
@@ -1,34 +1,22 @@
-/*
-   -------------------------------------------------------------------------------------
-   HX711_ADC
-   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
-
-
-/*
-   Settling time (number of samples) and data filtering can be adjusted in the config.h file
-   For calibration and storing the calibration value in eeprom, see example file "Calibration.ino"
-
-   The update() function checks for new data and starts the next conversion. In order to acheive maximum effective
-   sample rate, update() should be called at least as often as the HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS.
-   If you have other time consuming code running (i.e. a graphical LCD), consider calling update() from an interrupt routine,
-   see example file "Read_1x_load_cell_interrupt_driven.ino".
-
-*/
+.
 #include <Servo.h>
 #include <HX711_ADC.h>
 #if defined(ESP8266)|| defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
 unsigned long myTime;
-unsigned long lastTime = 2000;
+unsigned long lastTime = 6000;
 float lastWeight = 0;
 //pins:
 Servo servo;
-const int HX711_dout = 13; //mcu > HX711 dout pin
-const int HX711_sck = 12; //mcu > HX711 sck pin
+const int HX711_dout = 8; 
+const int HX711_sck = 9; 
 //HX711 constructor:
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
-
+int volume ; 
+int count = 0;
+int count1 = 0;
+float density ;
 const int calVal_eepromAdress = 0;
 unsigned long t = 0;
 static boolean newDataReady = 0;
@@ -40,16 +28,15 @@ void setup() {
 
 
   LoadCell.begin();
-  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
-  float calibrationValue; // calibration value (see example file "Calibration.ino")
-  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
+  float calibrationValue; 
+  calibrationValue = 624.38; 
+  
 #if defined(ESP8266)|| defined(ESP32)
-  //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
 #endif
-  EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+  EEPROM.get(calVal_eepromAdress, calibrationValue); 
 
-  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
-  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  unsigned long stabilizingtime = 2000;
+  boolean _tare = true; 
   LoadCell.start(stabilizingtime, _tare);
   if (LoadCell.getTareTimeoutFlag()) {
     Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
@@ -61,8 +48,8 @@ void setup() {
   }
   ////////////////////////////////////////////////////////////////////////////////////////////
   //increase value to slow down serial print activity
-
-  servo.attach(8);
+  volume = 63;
+  servo.attach(10);
   servo.write(90);
   delay(1000);
 }
@@ -70,38 +57,59 @@ void setup() {
 void loop() {
   myTime = millis();
 
-  // check for new data/start next conversion:
   if (LoadCell.update()) newDataReady = true;
-
-  // get smoothed value from the dataset:
+  
   if (newDataReady) {
     if (millis() > t + serialPrintInterval) {
       float i = LoadCell.getData();
-      Serial.print("Load_cell output val: ");
-      Serial.println(i);
+      Serial.println("Load_cell output val: " + String(i));
+      density = i/volume ;
+        Serial.println("Density: "+String(density));
+      //Serial.println(i);
       if (myTime > lastTime)
       {
-        lastTime = myTime + 1000;
-        if (abs(i - lastWeight) < 10)
+        lastTime = myTime + 6000;
+        
+        if (abs(i - lastWeight) < 1)
         {
-          if (i < 1) {
+          if (i < 1){
             servo.write(90);
           }
-          else if (i <= 50 || i >= 100) {
-            servo.write(0);
-          } else if (i >= 50 || i <= 100) {
-            servo.write(180);
           }
-        }
-        lastWeight = i;
+          while ((i >= 60 && i <= 80) && (density >=1.1 && density <=1.3) && count < 4) {
+            delay(2000);
+            servo.write(180);
+            delay(1000);
+            servo.write(90);
+            Serial.println("The block is ready for commercial use!");
+           count++;
+          }
+          while ((i>1 && i <= 60) || i >= 80 || (density <=1.1 && i > 1) || density >=1.3 && count1 < 4) {
+            delay(2000);
+            servo.write(0);
+            delay (1000);
+            servo.write(90);
+            count++;
+            Serial.println("There was a problem reagarding the construction of the block!");
+            if (i>1 && i <= 60){
+            Serial.println("Mass is low!");}
+              else if (i >= 80){
+              Serial.println("Mass is high!");}
+              else if (density <=1.1 && i>1)
+              { Serial.println("Density is low!");}
+              else if (density >=1.3){
+              Serial.println("Density is high!");
+              }
+          } 
+        
       }
-
+    }
       newDataReady = 0;
       t = millis();
-    
+
 
     }
   }
-}
 
-// receive command from serial terminal, send 't' to initiate tare operation:
+
+
